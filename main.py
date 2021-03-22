c454d15e853c4212bb06bb42a270eab50608cd21
Serhii Alieksieiev <se.alieksieiev@gmail.com>
2021-03-22 16:21:55 +0200

    Update main.py
    add check biltlink function

--- main.py
+++ main.py
@@ -1,42 +1,53 @@
 import requests
 
+
 def shorten_link(token, url):
-    headers = {
-        "Authorization": token
-    }
-    request_body = {
-        "long_url": url
-    }
     request_url = "https://api-ssl.bitly.com/v4/shorten"
     try:
-        response = requests.post(request_url, json=request_body, headers=headers)
+        response = requests.post(request_url, json=url, headers=token)
         response.raise_for_status()
         bitlink = response.json()['link']
         return bitlink
     except requests.exceptions.HTTPError as e:
-        print("Ошибка \n {}".format(e))
+        print("Ошибка1 \n {}".format(e))
 
 
 def count_clicks(token, url):
-    headers = {
-        "Authorization": token
-    }
-    params = {
-        "units" : -1
-    }
     request_url = f"https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks"
-    response = requests.get(request_url, params=params, headers=headers)
-    response.raise_for_status()
-    clicks_count = response.json()['link_clicks'][0]['clicks']
-    return clicks_count
+    try:
+        response = requests.get(request_url, headers=token)
+        response.raise_for_status()
+        clicks_count = response.json()['link_clicks'][0]['clicks']
+        return clicks_count
+    except requests.exceptions.HTTPError as e:
+        print("Ошибка2 \n {}".format(e))
+
+
+def check_bitlink(token, url):
+    reqest_url = f"https://api-ssl.bitly.com/v4/bitlinks/{url}"
+    try:
+        response = requests.get(reqest_url, headers=token)
+        response.raise_for_status()
+        return True
+    except requests.exceptions.HTTPError:
+        return False
+
 
 def main():
-    #long_url = input()
     bitly_token = "Bearer bbf5c519e9eb62def990efcdd45fd4493ec3f468"
-    bitlink = input()
+    link = input()
+    headers = {
+        "Authorization": bitly_token
+    }
+    request_body = {
+        "long_url": link
+    }
 
-    #print('Битлинк', shorten_link(bitly_token, long_url))
-    print('Количество кликов: ', count_clicks(bitly_token, bitlink))
+    if check_bitlink(headers, link):
+        print('Количество кликов: ', count_clicks(headers, link))
+    else:
+        print('Битлинк', shorten_link(headers, request_body))
+
 
 if __name__ == '__main__':
     main()
