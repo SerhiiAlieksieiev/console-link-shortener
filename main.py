0f4b0b4b7b8b44ac5ad543fa81e1c183abf7b4b1
Serhii Alieksieiev <se.alieksieiev@gmail.com>
2021-03-25 17:41:53 +0200

    Optimize code by move try except block to def main

--- main.py
+++ main.py
@@ -6,24 +6,18 @@ import requests
 
 def shorten_link(token, url):
     request_url = "https://api-ssl.bitly.com/v4/shorten"
-    try:
-        response = requests.post(request_url, json=url, headers=token)
-        response.raise_for_status()
-        bitlink = response.json()['link']
-        return bitlink
-    except requests.exceptions.HTTPError as e:
-        return "Ошибка \n {}".format(e)
+    response = requests.post(request_url, json=url, headers=token)
+    response.raise_for_status()
+    bitlink = response.json()['link']
+    return bitlink
 
 
 def count_clicks(token, url):
     request_url = f"https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks"
-    try:
-        response = requests.get(request_url, headers=token)
-        response.raise_for_status()
-        clicks_count = response.json()['link_clicks'][0]['clicks']
-        return clicks_count
-    except requests.exceptions.HTTPError as e:
-        return "Ошибка \n {}".format(e)
+    response = requests.get(request_url, headers=token)
+    response.raise_for_status()
+    clicks_count = response.json()['link_clicks'][0]['clicks']
+    return clicks_count
 
 
 def check_bitlink(token, url):
@@ -48,9 +42,15 @@ def main():
     }
 
     if check_bitlink(headers, link):
-        print('Количество кликов: ', count_clicks(headers, link))
+        try:
+            print('Количество кликов: ', count_clicks(headers, link))
+        except requests.exceptions.HTTPError as e:
+            print("Ошибка \n {}".format(e))
     else:
-        print('Битлинк', shorten_link(headers, body))
+        try:
+            print('Битлинк', shorten_link(headers, body))
+        except requests.exceptions.HTTPError as e:
+            print("Ошибка \n {}".format(e))
 
 
 if __name__ == '__main__':
